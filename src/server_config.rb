2dbefbc02df63f8ae792dffd8f061147b974a114
Tim Schofield <timothy.j.schofield@gmail.com>
2014-03-11 22:00:47 -0700

    Added server configuration class
    Included Rspec file and test config file

---
+++ src/server_config.rb
@@ -0,0 +1,56 @@
+require 'json'
+
+class ServerConfig
+  # Formatting options for JSON output
+  JSON_OPTS = {
+    :indent => '  ',
+    :space => ' ',
+    :space_before => ' ',
+    :object_nl => "\n"
+  }
+  
+  class FileNotFoundError < Exception; end
+  class InvalidOptionError < Exception; end
+  
+  attr_accessor :opts
+  
+  def initialize(config_file)
+    raise FileNotFoundError.new("Unable to find #{config_file}.") unless File.exists?(config_file)
+    @filename = config_file
+    @opts = {
+      path: '',
+      converter: ''
+    }
+    read_config
+  end
+  
+  def opts=(new_opts)
+    new_opts.each_pair do |key, value|
+      raise InvalidOptionError.new("#{key} is not a valid server option.") unless opts.has_key?(key)
+      @opts[key] = value
+    end
+  end
+  
+  def write
+    begin
+      file = File.open(@filename, 'w')
+      file.puts JSON.generate(self.opts, JSON_OPTS)
+      file.close
+    rescue Exception => e
+      raise e
+    end
+  end
+  
+  private
+  
+  def read_config
+    begin
+      file = File.open(@filename)
+      pending_opts = JSON.parse(file.read, symbolize_names: true)
+      file.close
+    rescue Exception => e
+      raise e
+    end
+    self.opts = pending_opts
+  end
+end
---
+++ test/spec_server_config.rb
@@ -0,0 +1,46 @@
+require_relative '../src/server_config.rb'
+
+describe ServerConfig do
+  it 'raises an exception when config file does not exist' do
+    filename = 'test/wrongfile.json'
+    expect{ServerConfig.new(filename)}.to raise_error(ServerConfig::FileNotFoundError)
+  end
+
+  it 'reads the main music directory from a json file' do
+    filename = 'test/config.json'
+    server_config = ServerConfig.new(filename)
+    expect(server_config.opts[:path]).to eq('/path/to/music') 
+  end
+
+  it 'allows new options to be written' do
+    filename = 'test/config.json'
+    server_config = ServerConfig.new(filename)
+    new_path = '/path/to/nowhere'
+    server_config.opts = {
+      path: new_path
+    }
+    expect(server_config.opts[:path]).to eq(new_path)
+  end
+
+  it 'does not allow nonexistant options to be written' do
+    filename = 'test/config.json'
+    server_config = ServerConfig.new(filename)
+    new_opts = {
+      apples: 'bananas'
+    }
+    expect{server_config.opts = new_opts}.to raise_error(ServerConfig::InvalidOptionError) 
+  end
+
+  it 'writes the config back to the original file' do
+    filename = 'test/config.json'
+    server_config = ServerConfig.new(filename)
+    old_path = server_config.opts[:path]
+    new_path = '/path/to/nowhere'
+    server_config.opts = { path: new_path }
+    server_config.write
+    new_config = ServerConfig.new(filename)
+    expect(new_config.opts[:path]).to eq('/path/to/nowhere')
+    server_config.opts = { path: old_path }
+    server_config.write
+  end
+end
