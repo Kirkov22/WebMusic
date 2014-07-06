168495988d76554b545e48332133260de1af8d11
Tim Schofield <timothy.j.schofield@gmail.com>
2014-07-06 14:52:12 -0700

    Integrated database with main app

--- app.rb
+++ app.rb
@@ -18,8 +18,9 @@ configure do
   set :converted, {}
   # TODO: Remove once MySQL database is set up
   include AppHelpers
-  songs = tagArray(settings.config.opts[:path])
-  set :songs, songs
+#   songs = tagArray(settings.config.opts[:path])
+#   set :songs, songs
+  set :database, startupDB(settings.config)
 end
 
 # Main Page
@@ -28,32 +29,40 @@ get '/' do
 end
 
 get '/getartists' do
-  first = /^#{params[:first_letter]}/i
-  selected = settings.songs.select { |song| song[:artist] =~ first }
-  artists = selected.map { |song| song[:artist] }
-  JSON.generate(artists.uniq)
+  first = params[:first_letter]
+#   first = /^#{params[:first_letter]}/i
+#   selected = settings.songs.select { |song| song[:artist] =~ first }
+#   artists = selected.map { |song| song[:artist] }
+  artists = settings.database.artists_by_letter(first)
+  JSON.generate(artists)
 end
 
 get '/getalbumsbyartist' do
   artist = params[:artist]
-  selected = settings.songs.select { |song| song[:artist] == artist }
-  albums = selected.map { |song| song[:album] }
+#   selected = settings.songs.select { |song| song[:artist] == artist }
+#   albums = selected.map { |song| song[:album] }
+  albums = settings.database.albums_by_artist(artist)
   JSON.generate(albums.uniq)
 end
 
 get '/getsongsbyalbum' do
+#   album = params[:album]
+# #   keepers = [:title, :id, :track]
+#   selected = settings.songs.select { |song| song[:album] == album }
+# #   selected.map { |song| song.keep_if { |k| keepers.include? k }}
+#   selected.sort_by! { |song| song[:track] }
   album = params[:album]
-#   keepers = [:title, :id, :track]
-  selected = settings.songs.select { |song| song[:album] == album }
-#   selected.map { |song| song.keep_if { |k| keepers.include? k }}
-  selected.sort_by! { |song| song[:track] }
-  JSON.generate(selected)
+  songs = settings.database.songs_by_album(album)
+  JSON.generate(songs)
+#   JSON.generate(selected)
 end
 
 get '/getsonginfo' do
   id = params[:id]
-  song = settings.songs.select { |song| song[:id] == id }
-  JSON.generate(song[0])
+#   song = settings.songs.select { |song| song[:id] == id }
+#   JSON.generate(song[0])
+  song = settings.database.song_info(id)
+  JSON.generate(song)
 end
 # Retrieve music file list
 # get '/getfilelist' do
@@ -67,17 +76,41 @@ end
 # Test for Javascript
 get '/music' do
   id = params[:songID]
-  if settings.converted.has_key?(id)
-    settings.converted[id]
+  song = settings.database.song_info(id)
+  ext = File.extname(song[:path]).downcase
+  
+  case ext
+  when Mpeg::EXT
+    audio = Mpeg.new(song[:path])
+  when Flac::EXT
+    audio = Flac.new(song[:path])
   else
-    selected_song = settings.songs.select { |song| song[:id] == id }
-    mp3 = Mpeg.new(selected_song[0][:path])
-    tempfile = settings.playNames.shift
-    settings.playNames.push(tempfile);
-    mp3.convert(settings.config.opts[:converter], "public/" + tempfile)
-    if settings.converted.length == 4
-      settings.converted.shift
-    end
-    settings.converted[id] = '/' + tempfile
+    audio = nil
   end
+  unless audio.nil?
+    if (settings.converted.has_key?(id))
+      settings.converted[id]
+    else
+      tempfile = settings.playNames.shift
+      settings.playNames.push(tempfile)
+      audio.convert(settings.config.opts[:converter], "public/" + tempfile)
+      if (settings.converted.length == 4)
+        settings.converted.shift
+      end
+      settings.converted[id] = '/' + tempfile
+    end
+  end
+#   if settings.converted.has_key?(id)
+#     settings.converted[id]
+#   else
+#     selected_song = settings.songs.select { |song| song[:id] == id }
+#     mp3 = Mpeg.new(selected_song[0][:path])
+#     tempfile = settings.playNames.shift
+#     settings.playNames.push(tempfile);
+#     mp3.convert(settings.config.opts[:converter], "public/" + tempfile)
+#     if settings.converted.length == 4
+#       settings.converted.shift
+#     end
+#     settings.converted[id] = '/' + tempfile
+#   end
 end
--- src/database.rb
+++ src/database.rb
@@ -46,44 +46,120 @@ class Database
       }
     end
   end
+  
+  def add_or_update_song(song)
+    song[:title]  ||= 'Unknown'
+    song[:artist] ||= 'Unknown'
+    song[:album]  ||= 'Unknown'
+    song[:track]  ||= 1
+    song[:year]   ||= Time.now.year
+    song[:path]   ||= '~/song.mp3'
+    
+    id = song_by_path(song[:path])
+    (id == -1) ? insert_song(song) : update_song(song, id)
+  end
+  
+  def artists_by_letter(letter)
+    result = query_artists_by_letter(letter)
+    [].tap do |artists|
+      result.each do |artist|
+        artists.push(artist[0]) 
+      end
+    end
+  end
+
+  def albums_by_artist(artist)
+    result = query_albums_by_artist(artist)
+    [].tap do |albums|
+      result.each do |album|
+        albums.push(album[0])
+      end
+    end
+  end
+  
+  def songs_by_album(album)
+    result = query_songs_by_album(album)
+    [].tap do |songs|
+      result.each do |song|
+        songs.push({
+          id:     song[0],
+          title:  song[1],
+          track:  song[2]
+        })
+      end
+    end
+  end
 
   private 
   
   attr_accessor :db
   
-  def find_artist(artist)
+  def query_with_result(query)
     @db.query_with_result = true
-    result = @db.query("SELECT a.artist_id FROM artists a WHERE a.name = '#{Mysql.quote(artist)}'")
+    @db.query(query)
+  end
+
+  def insert(query)
+    @db.query_with_result = false
+    @db.query(query)
+    @db.insert_id
+  end
+
+  def find_artist(artist)
+    query = "SELECT a.artist_id FROM artists a WHERE a.name = '#{Mysql.quote(artist)}'"
+    result = query_with_result(query)
     row = result.fetch_row
     row.nil? ? -1 : row[0].to_i
   end
   
   def insert_artist(artist)
-    @db.query_with_result = false
-    @db.query("INSERT IGNORE INTO artists (name) VALUES ('#{Mysql.quote(artist)}')")
-    @db.insert_id
+    query = "INSERT IGNORE INTO artists (name) VALUES ('#{Mysql.quote(artist)}')"
+    insert(query)
+  end
+
+  def query_artists_by_letter(letter)
+    query = "SELECT name FROM artists WHERE name "
+    if /[[:alpha:]]/.match(letter.chr)
+      query += "REGEXP '^#{letter.chr}'"
+    else
+      query += "NOT REGEXP '^[[:alpha:]]'"
+    end
+    query += " ORDER BY name"
+    query_with_result(query)
   end
   
   def find_album(album)
-    @db.query_with_result = true
-    result = @db.query("SELECT a.album_id FROM albums a WHERE a.name = '#{Mysql.quote(album)}'")
+    query = "SELECT a.album_id FROM albums a WHERE a.name = '#{Mysql.quote(album)}'"
+    result = query_with_result(query)
     row = result.fetch_row
     row.nil? ? -1 : row[0].to_i
   end
   
   def insert_album(album)
-    @db.query_with_result = false
-    @db.query("INSERT IGNORE INTO albums (name) VALUES ('#{Mysql.quote(album)}')")
-    @db.insert_id
+    query = "INSERT IGNORE INTO albums (name) VALUES ('#{Mysql.quote(album)}')"
+    insert(query)
   end
   
+  def query_albums_by_artist(artist)
+    query = <<ENDDOC
+SELECT alb.name FROM albums alb
+WHERE alb.album_id IN (
+  SELECT s.album_id FROM songs s
+  WHERE s.artist_id = (
+    SELECT art.artist_id FROM artists art
+    WHERE art.name = '#{Mysql.quote(artist)}'))
+ORDER BY alb.name
+ENDDOC
+    query_with_result(query)
+  end
+
   def song_by_path(path)
-    @db.query_with_result = true
-    result = @db.query("SELECT s.song_id FROM songs s WHERE s.path = '#{Mysql.quote(path)}'")
+    query = "SELECT s.song_id FROM songs s WHERE s.path = '#{Mysql.quote(path)}'"
+    result = query_with_result(query)
     row = result.fetch_row
     row.nil? ? -1 : row[0].to_i
   end
-
+  
   def song_by_id(id)
     query = <<ENDDOC
 SELECT s.title, art.name, alb.name, s.track_number, s.release_year, s.path
@@ -93,18 +169,21 @@ FROM songs s INNER JOIN artists art
   USING (album_id)
 WHERE s.song_id = #{id}
 ENDDOC
-    @db.query_with_result = true
-    @db.query(query)
+    query_with_result(query)
+  end
+
+  def query_songs_by_album(album)
+    query = <<ENDDOC
+SELECT s.song_id, s.title, s.track_number FROM songs s
+WHERE s.album_id = (
+  SELECT a.album_id FROM albums a
+  WHERE a.name = '#{Mysql.quote(album)}')
+ORDER BY s.track_number
+ENDDOC
+    query_with_result(query)
   end
 
   def insert_song(song)
-    song[:title]  ||= 'Unknown'
-    song[:artist] ||= 'Unknown'
-    song[:album]  ||= 'Unknown'
-    song[:track]  ||= 1
-    song[:year]   ||= Time.now.year
-    song[:path]   ||= '~/song.mp3'
-    
     artist_id = artist(song[:artist])
     album_id = album(song[:album])
     
@@ -113,9 +192,22 @@ INSERT IGNORE INTO songs (title, artist_id, album_id, track_number, release_year
 VALUES ('#{Mysql.quote(song[:title])}', #{artist_id}, #{album_id},
         #{song[:track]}, #{song[:year]}, '#{Mysql.quote(song[:path])}')
 ENDDOC
-    @db.query_with_result = false
-    @db.query(query)
-    @db.insert_id
+    insert(query)
   end
   
+  def update_song(song, id)
+    artist_id = artist(song[:artist])
+    album_id = album(song[:album])
+    
+    query = <<ENDDOC
+UPDATE songs
+SET title = '#{Mysql.quote(song[:title])}',
+    artist_id = #{artist_id},
+    album_id = #{album_id},
+    track_number = #{song[:track]},
+    release_year = #{song[:year]}
+WHERE song_id = #{id} 
+ENDDOC
+    insert(query)
+  end
 end
