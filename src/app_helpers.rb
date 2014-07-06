ae36c6692c885914994fec4c38ac806655f1bc87
Tim Schofield <timothy.j.schofield@gmail.com>
2014-07-06 14:51:36 -0700

    Removed functions moved to build_db.rb script

--- src/app_helpers.rb
+++ src/app_helpers.rb
@@ -8,54 +8,6 @@ module AppHelpers
 
   class FileNotFoundError < Exception; end
   
-  # Return a hash of tags for the music files at path
-  def tagArray(path)
-    [].tap do |taglist|
-      i = 0
-      listMusicFiles(path).each do |file|
-        ext = File.extname(file).downcase
-        
-        case ext
-        when Mpeg::EXT
-          song = Mpeg.new(file).tag.clone
-        when Flac::EXT
-          song = Flac.new(file).tag.clone
-        else
-          song = {
-            artist:  "Unhandled extension - #{ext}",
-            album:   "",
-            title:   "",
-            year:    "",
-            track:   "",
-            genre:   "",
-            comment: ""
-          }
-        end
-        
-        # TODO: Assign each song an ID through MySQL
-        id = i.to_s
-#         id = i.to_s.rjust(5,'0').to_sym
-        
-        # TODO: Remove these lines once path & id are in MySQL
-        song[:id] = id
-        song[:path] = file
-        
-        i += 1
-        taglist.push(song)
-      end
-    end
-  end
-  
-  # Return an array of music files at path
-  def listMusicFiles(path)
-    Dir.glob(path + "**/*{" + Mpeg::EXT + "," + Flac::EXT + "}", caseinsensitive)
-  end
-  
-  # Flag for case insensitive glob expression
-  def caseinsensitive
-    File::FNM_CASEFOLD
-  end
-  
   def startupDB(server_config)
     opts = {
       host:   'localhost',
@@ -70,7 +22,7 @@ module AppHelpers
 
   def getMySQLSocket(mysql_config)
     # expects config file to have the following contents:
-    # [mysqld]
+    # [client]
     # ...
     # socket = <path to socket>
 
@@ -85,7 +37,7 @@ module AppHelpers
 
   def getMySQLPort(mysql_config)
     # expects config file to have the following contents:
-    # [mysqld]
+    # [client]
     # ...
     # port = <port number>
 
